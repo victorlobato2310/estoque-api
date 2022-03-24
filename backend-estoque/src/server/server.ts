7f1586e6878268e530596beaf4dd802e724b3c1f
Victor Lobato <victorlobato2310@gmail.com>
2022-03-24 16:16:18 -0300

    Adicionando cors ao projeto

--- backend-estoque/src/server/server.ts
+++ backend-estoque/src/server/server.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import { conn } from '../database/config';
 import estoqueRouter from '../routes/estoque.routes';
 import usuarioRouter from '../routes/usuario.routes';
+import cors from 'cors';
 
 const app = express();
 app.use(express.json());
@@ -10,6 +11,11 @@ app.use('/estoque/produtos/api/v1', estoqueRouter);
 app.use('/usuarios', usuarioRouter);
 
 app.use(conn);
+
+const allowedOrigins = ["http://localhost:4200"];
+const options: cors.CorsOptions = {
+  origin: allowedOrigins,
+};
 
 app.listen(3000, ()=> {
     console.log('Está rodando.');
